4cbeddfd5daa39e33fc4a7a96523805fec78f1f9
Alex Oyebamiji <74204083+Lex043@users.noreply.github.com>
2022-08-03 15:26:21 +0100

    Update

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Intro = () => {
   return (
     <>
-      <div className="h-auto py-9 px-10 bg-black text-white text-center font-Lobster">
+      <div className="h-auto py-9 px-10 bg-black text-white text-center">
         <p>
           E-Millenial Store is an hypothetical online store for gadgets we
           believe every 'Millenial' or 'Gen-Z' should have today. We've got
