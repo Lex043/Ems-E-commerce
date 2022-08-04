15647203aa9e5fdc722582fc2be89c713a631041
Oyebamiji Alex <alexferanmi390@gmail.com>
2022-08-04 01:19:52 +0100

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import ems from "../assets/EMS.svg";
 const Footer = () => {
   return (
     <>
-      <div className="bg-black px-10 py-10 h-auto flex justify-between gap-4 items-center">
+      <div className="bg-black px-5 h-auto flex justify-between gap-4 items-center">
         <div>
-          <img src={ems} alt="logo" className="w-24" />
+          <img src={ems} alt="logo" className="w-24 h-24" />
         </div>
         <div className="text-white font-Lobster">
           <p>Designed and Crafted by Lex043</p>
