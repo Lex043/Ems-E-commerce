d79988244bb5217d9a6a76c01c5233832a54aff6
Oyebamiji Alex <alexferanmi390@gmail.com>
2022-08-04 11:51:48 +0100

    fix: responsiveness

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -9,12 +9,12 @@ const Products = () => {
 
   return (
     <>
-      <div className="pt-5 pb-9 h-full">
+      <div className="h-auto flex flex-col mx-auto">
         <div className="text-black font-bold text-2xl text-center">
           <h1>OUR GADGETS</h1>
         </div>
 
-        <div className="grid grid-cols-1 justify-center gap-6 pt-5 px-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid h-full grid-cols-1 justify-center gap-6 pt-5 px-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {goods.map((good) => (
             <div key={good.id}>
               <div className="w-full px-3">
